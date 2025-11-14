250e815cad9589659a64e4e35e3e98634b8e3c13
dogunfx <dogunfx@gmail.com>
2025-11-14 12:21:51 +0100

    completed site details

--- global/constant.ts
+++ global/constant.ts
@@ -1,20 +1,27 @@
-export const SITE_NAME: string = ' Digital Galaxy Trading ';
-export const SITE_URL: string = 'digitalgalaxytrading.com';
+export const SITE_NAME: string = ' Satrix Trade Hub ';
+export const SITE_URL: string = 'satrixtradehub.com';
 export const SITE_URL_WITH_HTTPS: string = `https://${SITE_URL}`;
 export const SITE_ADDRESS: string = 'USA';
-export const SITE_DASHBOARD: string = 'https://app.digitalgalaxytrading.com';
-export const SITE_EMAIL: string = 'digitalgalaxytrading@lh44dmgt.com';
+export const SITE_DASHBOARD: string = 'https://app.satrixtradehub.com';
+export const SITE_EMAIL: string = 'support@satrixtradehub.com';
 export const SITE_PHONE: string = 'USA';
 export const SITE_TESTIMONIES: string =
-  'https://app.digitalgalaxytrading.com/api/testimonies';
+  'https://app.satrixtradehub.com/api/testimonies';
 
 export const LIVE_CHAT = `
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '40ee1454cf56e26920661e4d42936750a3090aaa';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = '2cacdc8e5aecb1b5c29dc958b2f80444e381de25';
+    window.smartsupp || (function(d) {
+        var s, c, o = smartsupp = function() {
+            o._.push(arguments)
+        };
+        o._ = [];
+        s = d.getElementsByTagName('script')[0];
+        c = d.createElement('script');
+        c.type = 'text/javascript';
+        c.charset = 'utf-8';
+        c.async = true;
+        c.src = 'https://www.smartsuppchat.com/loader.js?';
+        s.parentNode.insertBefore(c, s);
+    })(document);
   `;
